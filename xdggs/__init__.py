997e328b4a981fc49efc85199955b5d2fe1c4096
Benoit Bovy <benbovy@gmail.com>
2023-11-07 09:33:25 +0100

    boilerplate

--- xdggs/__init__.py
+++ xdggs/__init__.py
@@ -1,7 +1,14 @@
-from .accessor import DGGSAccessor
+from importlib.metadata import PackageNotFoundError, version
+
+from .accessor import DGGSAccessor   # noqa
 from .index import DGGSIndex
 from .healpix import HealpixIndex
 
-__all__ = ["DGGSIndex", "HealpixIndex"]
 
-__version__ = "0.0.1"
+try:
+    __version__ = version("xdggs")
+except PackageNotFoundError:  # noqa
+    # package is not installed
+    pass
+
+__all__ = ["__version__", "DGGSIndex", "HealpixIndex"]
