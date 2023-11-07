b259924578b055acc34bc8b52aa88af4b8b4477f
Benoit Bovy <benbovy@gmail.com>
2023-11-07 12:13:19 +0100

    Add H3index (#4)
    * add H3Index
    
    * update notebook examples

--- xdggs/__init__.py
+++ xdggs/__init__.py
@@ -1,6 +1,7 @@
 from importlib.metadata import PackageNotFoundError, version
 
 from xdggs.accessor import DGGSAccessor  # noqa
+from xdggs.h3 import H3Index
 from xdggs.healpix import HealpixIndex
 from xdggs.index import DGGSIndex
 
@@ -10,4 +11,4 @@ except PackageNotFoundError:  # noqa
     # package is not installed
     __version__ = "9999"
 
-__all__ = ["__version__", "DGGSIndex", "HealpixIndex"]
+__all__ = ["__version__", "DGGSIndex", "H3Index", "HealpixIndex"]
