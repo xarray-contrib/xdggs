945076b217f3498b302b35f6d0a864177b5c6e9d
Justus Magin <keewis@posteo.de>
2023-11-29 11:08:13 +0100

    add tests for `_replace`

--- xdggs/tests/test_h3.py
+++ xdggs/tests/test_h3.py
@@ -1,6 +1,9 @@
+import itertools
+
 import numpy as np
 import pytest
 import xarray as xr
+from xarray.core.indexes import PandasIndex
 
 from xdggs import h3
 
@@ -26,6 +29,9 @@ variables = [
     xr.Variable(
         dims[1], cell_ids[2], {"grid_type": "h3", "resolution": resolutions[2]}
     ),
+]
+variable_combinations = [
+    (old, new) for old, new in itertools.product(variables, repeat=2)
 ]
 
 
@@ -58,3 +64,21 @@ def test_from_variables(variable_name, variable, options):
     # TODO: how do we check the index, if at all?
     assert (index._pd_index.dim,) == variable.dims
     assert np.all(index._pd_index.index.values == variable.data)
+
+
+@pytest.mark.parametrize(["old_variable", "new_variable"], variable_combinations)
+def test_replace(old_variable, new_variable):
+    index = h3.H3Index(
+        cell_ids=old_variable.data,
+        dim=old_variable.dims[0],
+        resolution=old_variable.attrs["resolution"],
+    )
+    new_pandas_index = PandasIndex.from_variables(
+        {"cell_ids": new_variable}, options={}
+    )
+
+    new_index = index._replace(new_pandas_index)
+
+    assert new_index._resolution == index._resolution
+    assert new_index._dim == index._dim
+    assert new_index._pd_index == new_pandas_index
