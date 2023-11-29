f6ecf48c3fdd39613179637a1e1566b390c37e5b
Justus Magin <keewis@posteo.de>
2023-11-29 10:42:10 +0100

    add tests for `test_from_variables`

--- xdggs/tests/test_h3.py
+++ xdggs/tests/test_h3.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pytest
+import xarray as xr
 
 from xdggs import h3
 
@@ -11,6 +12,15 @@ cell_ids = [
 dims = ["cells", "zones"]
 resolutions = [1, 5, 15]
 variable_names = ["cell_ids", "zonal_ids", "zone_ids"]
+
+variables = [
+    xr.Variable(
+        dims[0], cell_ids[0], {"grid_type": "h3", "resolution": resolutions[0]}
+    ),
+    xr.Variable(
+        dims[1], cell_ids[1], {"grid_type": "h3", "resolution": resolutions[2]}
+    ),
+]
 
 
 @pytest.mark.parametrize("resolution", resolutions)
@@ -25,3 +35,18 @@ def test_init(cell_ids, dim, resolution):
     # TODO: how do we check the index, if at all?
     assert index._pd_index.dim == dim
     assert np.all(index._pd_index.index.values == cell_ids)
+
+
+@pytest.mark.parametrize("variable", variables)
+@pytest.mark.parametrize("variable_name", variable_names)
+@pytest.mark.parametrize("options", [{}])
+def test_from_variables(variable_name, variable, options):
+    variables = {variable_name: variable}
+    index = h3.H3Index.from_variables(variables, options=options)
+
+    assert index._resolution == variable.attrs["resolution"]
+    assert (index._dim,) == variable.dims
+
+    # TODO: how do we check the index, if at all?
+    assert (index._pd_index.dim,) == variable.dims
+    assert np.all(index._pd_index.index.values == variable.data)
