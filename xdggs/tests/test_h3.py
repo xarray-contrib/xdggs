280c3364e7bda454a4006d454222c33867efde71
Justus Magin <keewis@posteo.de>
2023-11-29 10:37:10 +0100

    use variables to store the test data

--- xdggs/tests/test_h3.py
+++ xdggs/tests/test_h3.py
@@ -3,14 +3,25 @@ import pytest
 
 from xdggs import h3
 
+cell_ids = [
+    np.array([0x832830FFFFFFFFF]),
+    np.array([0x832831FFFFFFFFF, 0x832832FFFFFFFFF]),
+    np.array([0x832833FFFFFFFFF, 0x832834FFFFFFFFF, 0x832835FFFFFFFFF]),
+]
+dims = ["cells", "zones"]
+resolutions = [1, 5, 15]
+variable_names = ["cell_ids", "zonal_ids", "zone_ids"]
 
-@pytest.mark.parametrize("resolution", [1, 10, 15])
-@pytest.mark.parametrize("dim", ["cells", "zones"])
-@pytest.mark.parametrize("cell_ids", (np.arange(0, 5), np.arange(5, 10)))
+
+@pytest.mark.parametrize("resolution", resolutions)
+@pytest.mark.parametrize("dim", dims)
+@pytest.mark.parametrize("cell_ids", cell_ids)
 def test_init(cell_ids, dim, resolution):
     index = h3.H3Index(cell_ids, dim, resolution)
 
     assert index._resolution == resolution
     assert index._dim == dim
+
+    # TODO: how do we check the index, if at all?
     assert index._pd_index.dim == dim
     assert np.all(index._pd_index.index.values == cell_ids)
