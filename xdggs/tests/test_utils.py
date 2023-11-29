a89c7a2f49a0aa7b2204580bcb6edd88926089f7
Justus Magin <keewis@posteo.de>
2023-11-29 12:06:44 +0100

    add tests for the cell id variable extraction function

--- xdggs/tests/test_utils.py
+++ xdggs/tests/test_utils.py
@@ -1,4 +1,5 @@
 import pytest
+import xarray as xr
 
 import xdggs.utils
 
@@ -16,3 +17,26 @@ def test_register_dggs(monkeypatch, name):
 
     assert len(registry) == 1
     assert name in registry and registry[name] is grid
+
+
+@pytest.mark.parametrize(
+    ["variables", "expected"],
+    (
+        (
+            {"cell_ids": xr.Variable("cells", [0, 1])},
+            ("cell_ids", "cells"),
+        ),
+        (
+            {"zone_ids": xr.Variable("zones", [0, 1])},
+            ("zone_ids", "zones"),
+        ),
+    ),
+)
+def test_extract_cell_id_variable(variables, expected):
+    expected_var = variables[expected[0]]
+
+    actual = xdggs.utils._extract_cell_id_variable(variables)
+    actual_var = actual[1]
+
+    assert actual[::2] == expected
+    xr.testing.assert_equal(actual_var, expected_var)
