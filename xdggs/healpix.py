10b0c7e4a5ea217815817ed208b388ccd43abeba
Justus Magin <keewis@posteo.de>
2023-12-13 14:30:36 +0100

    integrate the grid info object into the healpix index

--- xdggs/healpix.py
+++ xdggs/healpix.py
@@ -94,15 +94,10 @@ class HealpixIndex(DGGSIndex):
         self,
         cell_ids: Any | PandasIndex,
         dim: str,
-        nside: int,
-        nest: bool,
-        rot_latlon: tuple[float, float],
+        grid_info,
     ):
         super().__init__(cell_ids, dim)
-
-        self._nside = nside
-        self._nest = nest
-        self._rot_latlon = rot_latlon
+        self._grid = grid_info
 
     @classmethod
     def from_variables(
@@ -113,23 +108,25 @@ class HealpixIndex(DGGSIndex):
     ) -> "HealpixIndex":
         _, var, dim = _extract_cell_id_variable(variables)
 
-        nside = var.attrs.get("nside", options.get("nside"))
-        nest = var.attrs.get("nest", options.get("nest", False))
-        rot_latlon = var.attrs.get("rot_latlon", options.get("rot_latlon", (0.0, 0.0)))
+        grid_info = HealpixInfo.from_dict(var.attrs | options)
 
-        return cls(var.data, dim, nside, nest, rot_latlon)
+        return cls(var.data, dim, grid_info)
 
     def _replace(self, new_pd_index: PandasIndex):
-        return type(self)(
-            new_pd_index, self._dim, self._nside, self._nest, self._rot_latlon
-        )
+        return type(self)(new_pd_index, self._dim, self._grid)
 
     def _latlon2cellid(self, lat: Any, lon: Any) -> np.ndarray:
-        return healpy.ang2pix(self._nside, lon, lat, lonlat=True, nest=self._nest)
+        # TODO apply rotation
+        return healpy.ang2pix(
+            self._grid.nside, lon, lat, lonlat=True, nest=self._grid.nest
+        )
 
     def _cellid2latlon(self, cell_ids: Any) -> tuple[np.ndarray, np.ndarray]:
-        lon, lat = healpy.pix2ang(self._nside, cell_ids, nest=self._nest, lonlat=True)
-        return np.stack([lon, lat], axis=-1)
+        lon, lat = healpy.pix2ang(
+            self._grid.nside, cell_ids, nest=self._grid.nest, lonlat=True
+        )
+        # TODO: apply rotation
+        return np.stack([lon, lat], axis=0)
 
     def _repr_inline_(self, max_width: int):
-        return f"HealpixIndex(nside={self._nside}, nest={self._nest}, rot_latlon={self._rot_latlon!r})"
+        return f"HealpixIndex(nside={self._grid.resolution}, indexing_scheme={self._grid.indexing_scheme}, rotation={self._grid.rotation!r})"
