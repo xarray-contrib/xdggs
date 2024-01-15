2c3b2cbf7c39ab075a5b9528ea5f2cd13821d9f8
Justus Magin <keewis@posteo.de>
2024-01-15 11:21:35 +0100

    add `to_dict` as a new required method

--- xdggs/grid.py
+++ xdggs/grid.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import TypeVar
+from typing import Any, Self, TypeVar
 
 T = TypeVar("T")
 
@@ -9,5 +9,8 @@ class DGGSInfo:
     resolution: int
 
     @classmethod
-    def from_dict(cls: type[T], mapping: dict) -> T:
+    def from_dict(cls: type[T], mapping: dict[str, Any]) -> T:
         return cls(**mapping)
+
+    def to_dict(self: Self) -> dict[str, Any]:
+        return {"resolution": self.resolution}
