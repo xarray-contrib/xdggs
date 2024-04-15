c8525e52de561e58c85089c753e5fe351bbc16c7
Justus Magin <keewis@posteo.de>
2024-04-15 11:48:55 +0200

    add very limited tests

---
+++ xdggs/tests/test_index.py
@@ -0,0 +1,28 @@
+import pytest
+import xarray as xr
+
+from xdggs import index
+
+
+@pytest.fixture
+def dggs_example():
+    return xr.Dataset(
+        coords={"cell_ids": ("cells", [0, 1], {"grid_name": "test", "resolution": 2})}
+    )
+
+
+def test_create_index(dggs_example):
+    # TODO: improve unknown index message
+    with pytest.raises(KeyError, match="test"):
+        dggs_example.set_xindex("cell_ids", index.DGGSIndex)
+
+
+def test_decode(dggs_example):
+    # TODO: improve unknown index message
+    with pytest.raises(KeyError, match="test"):
+        dggs_example.pipe(index.decode)
+
+
+def test_decode_indexed(dggs_example):
+    with pytest.raises(KeyError, match="test"):
+        dggs_example.set_xindex("cell_ids").pipe(index.decode)
