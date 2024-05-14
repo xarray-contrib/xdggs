873a9c956ac838668aef21e0c918478ed371bd7f
Justus Magin <keewis@posteo.de>
2024-05-14 14:53:34 +0200

    move the match tests into a test class

--- xdggs/tests/test_matchers.py
+++ xdggs/tests/test_matchers.py
@@ -8,35 +8,42 @@ except NameError:
     from exceptiongroup import ExceptionGroup
 
 
-def test_construct_simple():
-    # simple
-    matchers.Match(ValueError)
-    matchers.Match((ValueError, NameError))
-    matchers.Match(ExceptionGroup)
-    with pytest.raises(
-        TypeError, match="exception type must be one or more exceptions"
-    ):
-        matchers.Match(int)
+class TestMatch:
+    def test_construct_simple(self):
+        # simple
+        matchers.Match(ValueError)
+        matchers.Match((ValueError, NameError))
+        matchers.Match(ExceptionGroup)
+        with pytest.raises(
+            TypeError, match="exception type must be one or more exceptions"
+        ):
+            matchers.Match(int)
 
-    # with match string
-    matchers.Match(TypeError, match="pattern")
-    matchers.Match(ExceptionGroup, match="pattern")
-    with pytest.raises(TypeError):
-        matchers.Match(ValueError, match=int)
+        # with match string
+        matchers.Match(TypeError, match="pattern")
+        matchers.Match(ExceptionGroup, match="pattern")
+        with pytest.raises(TypeError):
+            matchers.Match(ValueError, match=int)
 
-    # with submatchers
-    with pytest.raises(TypeError):
-        matchers.Match(ValueError, submatchers=[matchers.Match(ValueError)])
+        # with submatchers
+        with pytest.raises(TypeError):
+            matchers.Match(ValueError, submatchers=[matchers.Match(ValueError)])
 
-
-@pytest.mark.parametrize(
-    ["mapping", "args", "kwargs"],
-    (({"exceptions": ValueError}, (ValueError,), {}),),
-)
-def test_from_dict(mapping, args, kwargs):
-    actual = matchers.Match.from_dict(mapping)
-    expected = matchers.Match(*args, **kwargs)
-    assert actual == expected
+    @pytest.mark.parametrize(
+        ["mapping", "args", "kwargs"],
+        (
+            ({"exceptions": ValueError}, (ValueError,), {}),
+            (
+                {"exceptions": ValueError, "match": "abc"},
+                (ValueError,),
+                {"match": "abc"},
+            ),
+        ),
+    )
+    def test_from_dict(self, mapping, args, kwargs):
+        actual = matchers.Match.from_dict(mapping)
+        expected = matchers.Match(*args, **kwargs)
+        assert actual == expected
 
 
 def test_assert_exceptions_equal():
