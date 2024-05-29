1fc298f826a01e74ddbc416e645f5f62a403587d
Justus Magin <keewis@posteo.de>
2024-05-29 20:30:11 +0200

    expect `ValueError` instead of `KeyError`

--- xdggs/tests/test_index.py
+++ xdggs/tests/test_index.py
@@ -13,16 +13,16 @@ def dggs_example():
 
 def test_create_index(dggs_example):
     # TODO: improve unknown index message
-    with pytest.raises(KeyError, match="test"):
+    with pytest.raises(ValueError, match="test"):
         dggs_example.set_xindex("cell_ids", index.DGGSIndex)
 
 
 def test_decode(dggs_example):
     # TODO: improve unknown index message
-    with pytest.raises(KeyError, match="test"):
+    with pytest.raises(ValueError, match="test"):
         dggs_example.pipe(index.decode)
 
 
 def test_decode_indexed(dggs_example):
-    with pytest.raises(KeyError, match="test"):
+    with pytest.raises(ValueError, match="test"):
         dggs_example.set_xindex("cell_ids").pipe(index.decode)
