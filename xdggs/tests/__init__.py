b635c99de44eeac8c501e8e6fce13a9171ff6a35
Justus Magin <keewis@posteo.de>
2024-05-29 17:58:58 +0200

    use the `matchers` module instead

--- xdggs/tests/__init__.py
+++ xdggs/tests/__init__.py
@@ -1,35 +1,5 @@
-import enum
-import re
-from dataclasses import dataclass, field
-
-try:
-    ExceptionGroup
-except NameError:  # pragma: no cover
-    from exceptiongroup import ExceptionGroup
-
-
-class MatchResult(enum.Enum):
-    match = 0
-    mismatched_typing = 1
-    mismatched_message = 2
-
-
-# necessary until pytest-dev/pytest#11538 is resolved
-@dataclass
-class Match:
-    exc: Exception | ExceptionGroup
-    submatchers: list = field(default_factory=list)
-    match: str = None
-
-    def __post_init__(self):
-        if not isinstance(self.exc, ExceptionGroup) and self.submatchers:
-            raise TypeError("can only pass sub-matchers for exception groups")
-
-    def assert_match(self, exc):
-        assert type(exc) is type(self.exc)  # noqa: E721
-
-        assert re.search(str(exc), self.match), "message does not match"
-
-
-def assert_exceptions_equal(actual, expected):
-    assert type(actual) is type(expected)
+from xdggs.tests.matchers import (  # noqa: F401
+    Match,
+    MatchResult,
+    assert_exceptions_equal,
+)
