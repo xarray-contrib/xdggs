b2454db84b1374a951e2d214898234bf1180f4de
Justus Magin <keewis@posteo.de>
2024-06-10 16:37:39 +0200

    avoid re-evaluating the key for each item

--- xdggs/itertools.py
+++ xdggs/itertools.py
@@ -1,9 +1,17 @@
 import itertools
 
 
+def first(x):
+    return x[0]
+
+
 def identity(x):
     return x
 
 
 def groupby(iterable, key):
-    return itertools.groupby(sorted(iterable, key=key), key=key)
+    # to avoid re-evaluating `key` twice for each item, which might be expensive
+    applied = ((key(item), item) for item in iterable)
+    sorted_ = sorted(applied, key=first)
+
+    return itertools.groupby(sorted_, key=first)
