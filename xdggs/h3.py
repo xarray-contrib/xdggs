fix the cell centers computation (#61)
* expose the grid info object

* use the grid info methods to implement the reconstruction of cell ids

* add a method to retrieve the cell centers using the accessor

* forward to the grid object

* remove the left-over conversion methods on the index base class

* add a test to make sure the accessor returns the right cell centers

* inverse the order of coordinates for H3

* fix the old `assign_latlon_coords` method

* change the expectations in the h3 tests

now we always expect longitude, then latitude as a tuple

xdggs/h3.py
@@ -38,7 +38,9 @@ class H3Info(DGGSInfo):
     def cell_ids2geographic(
         self, cell_ids: np.ndarray
     ) -> tuple[np.ndarray, np.ndarray]:
-        return cells_to_coordinates(cell_ids, radians=False)
+        lat, lon = cells_to_coordinates(cell_ids, radians=False)
+
+        return lon, lat
 
     def geographic2cell_ids(self, lon, lat):
         return coordinates_to_cells(lat, lon, self.resolution, radians=False)