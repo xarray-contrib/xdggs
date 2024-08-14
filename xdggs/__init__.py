0eb98067220e9ae0170e5c83a73d144df6e8d90d
Justus Magin <keewis@posteo.de>
2024-08-14 15:15:16 +0200

    expose DGGSInfo

--- xdggs/__init__.py
+++ xdggs/__init__.py
@@ -1,6 +1,7 @@
 from importlib.metadata import PackageNotFoundError, version
 
 from xdggs.accessor import DGGSAccessor  # noqa: F401
+from xdggs.grid import DGGSInfo
 from xdggs.h3 import H3Index
 from xdggs.healpix import HealpixIndex
 from xdggs.index import DGGSIndex, decode
@@ -11,4 +12,4 @@ except PackageNotFoundError:  # noqa # pragma: no cover
     # package is not installed
     __version__ = "9999"
 
-__all__ = ["__version__", "DGGSIndex", "H3Index", "HealpixIndex", "decode"]
+__all__ = ["__version__", "DGGSIndex", "H3Index", "HealpixIndex", "decode", "DGGSInfo"]
