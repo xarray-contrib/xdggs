4678c784303e189464fd3770ee592be8eaff10ec
Justus Magin <keewis@posteo.de>
2024-09-26 12:22:05 +0200

    rename for h3

--- xdggs/h3.py
+++ xdggs/h3.py
@@ -7,6 +7,8 @@ try:
 except ImportError:  # pragma: no cover
     from typing_extensions import Self
 
+import operator
+
 import numpy as np
 import xarray as xr
 from h3ronpy.arrow.vector import cells_to_coordinates, coordinates_to_cells
@@ -14,26 +16,57 @@ from xarray.indexes import PandasIndex
 
 from xdggs.grid import DGGSInfo
 from xdggs.index import DGGSIndex
+from xdggs.itertools import groupby, identity
 from xdggs.utils import _extract_cell_id_variable, register_dggs
 
 
 @dataclass(frozen=True)
 class H3Info(DGGSInfo):
-    resolution: int
+    level: int
 
-    valid_parameters: ClassVar[dict[str, Any]] = {"resolution": range(16)}
+    valid_parameters: ClassVar[dict[str, Any]] = {"level": range(16)}
 
     def __post_init__(self):
-        if self.resolution not in self.valid_parameters["resolution"]:
-            raise ValueError("resolution must be an integer between 0 and 15")
+        if self.level not in self.valid_parameters["level"]:
+            raise ValueError("level must be an integer between 0 and 15")
 
     @classmethod
     def from_dict(cls: type[Self], mapping: dict[str, Any]) -> Self:
-        params = {k: v for k, v in mapping.items() if k != "grid_name"}
+        translations = {
+            "resolution": ("level", identity),
+        }
+
+        def translate(name, value):
+            new_name, translator = translations.get(name, (name, identity))
+
+            return new_name, name, translator(value)
+
+        translated = (translate(name, value) for name, value in mapping.items())
+        grouped = {
+            name: [(old_name, value) for _, old_name, value in group]
+            for name, group in groupby(translated, key=operator.itemgetter(0))
+        }
+        duplicated_parameters = {
+            name: group for name, group in grouped.items() if len(group) != 1
+        }
+        if duplicated_parameters:
+            raise ExceptionGroup(
+                "received multiple values for parameters",
+                [
+                    ValueError(
+                        f"Parameter {name} received multiple values: {sorted(n for n, _ in group)}"
+                    )
+                    for name, group in duplicated_parameters.items()
+                ],
+            )
+
+        params = {
+            name: group[0][1] for name, group in grouped.items() if name != "grid_name"
+        }
         return cls(**params)
 
     def to_dict(self: Self) -> dict[str, Any]:
-        return {"grid_name": "h3", "resolution": self.resolution}
+        return {"grid_name": "h3", "level": self.level}
 
     def cell_ids2geographic(
         self, cell_ids: np.ndarray
@@ -43,7 +76,7 @@ class H3Info(DGGSInfo):
         return lon, lat
 
     def geographic2cell_ids(self, lon, lat):
-        return coordinates_to_cells(lat, lon, self.resolution, radians=False)
+        return coordinates_to_cells(lat, lon, self.level, radians=False)
 
 
 @register_dggs("h3")
@@ -79,4 +112,4 @@ class H3Index(DGGSIndex):
         return type(self)(new_pd_index, self._dim, self._grid)
 
     def _repr_inline_(self, max_width: int):
-        return f"H3Index(resolution={self._grid.resolution})"
+        return f"H3Index(level={self._grid.level})"
