302a97b2abf3b509b716b3d56c04919895b4c208
Justus Magin <keewis@posteo.de>
2024-10-01 16:23:10 +0200

    draft of the explore functionality based on `lonboard`

---
+++ xdggs/plotting.py
@@ -0,0 +1,54 @@
+import numpy as np
+
+
+def explore(
+    arr,
+    cell_boundaries="cell_boundaries",
+    cell_dim="cells",
+    cmap="viridis",
+    center=None,
+):
+    import geopandas as gpd
+    import lonboard
+    from lonboard import SolidPolygonLayer
+    from lonboard.colormap import apply_continuous_cmap
+    from matplotlib import colormaps
+    from matplotlib.colors import CenteredNorm, Normalizer
+
+    if len(arr.dims) != 1 or cell_dim not in arr.dims:
+        raise ValueError(
+            f"exploration only works with a single dimension ('{cell_dim}')"
+        )
+
+    if cell_boundaries not in arr.coords:
+        raise ValueError(
+            f"cannot find the cell boundaries coordinate: '{cell_boundaries}'"
+        )
+
+    name = arr.name or "__temporary_name__"
+
+    gdf = (
+        arr.to_dataset(name=name)
+        .to_pandas()
+        .pipe(gpd.GeoDataFrame, geometry=cell_boundaries, crs=4326)
+    )
+
+    data = gdf[name].to_numpy()
+
+    if center is None:
+        normalizer = Normalizer()
+    else:
+        min_ = np.min(data)
+        max_ = np.max(data)
+
+        halfrange = max(abs(min_ - center), abs(max_ - center))
+        normalizer = CenteredNorm(vcenter=center, halfrange=halfrange)
+
+    normalized_data = normalizer(data)
+
+    colormap = colormaps[cmap]
+    colors = apply_continuous_cmap(normalized_data, colormap)
+
+    layer = SolidPolygonLayer.from_geopandas(gdf, filled=True, get_fill_color=colors)
+
+    return lonboard.Map(layer)
