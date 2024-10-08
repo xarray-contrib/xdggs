2ef3192445a0b64e3dc22d8bfd21e0743137e20d
Justus Magin <keewis@posteo.de>
2024-10-08 11:40:35 +0200

    move the healpix-specific code to `xdggs.healpix`
    In the future, we might want to move the conversion code to a separate module.

--- xdggs/plotting.py
+++ xdggs/plotting.py
@@ -1,77 +1,6 @@
 import numpy as np
 
 
-def center_around_prime_meridian(lon, lat):
-    # three tasks:
-    # - center around the prime meridian (map to a range of [-180, 180])
-    # - replace the longitude of points at the poles with the median
-    #   of longitude of the other vertices
-    # - cells that cross the dateline should have longitudes around 180
-
-    # center around prime meridian
-    recentered = (lon + 180) % 360 - 180
-
-    # replace lon of pole with the median of the remaining vertices
-    contains_poles = np.isin(lat, np.array([-90, 90]))
-    pole_cells = np.any(contains_poles, axis=-1)
-    recentered[contains_poles] = np.median(
-        np.reshape(
-            recentered[pole_cells[:, None] & np.logical_not(contains_poles)], (-1, 3)
-        ),
-        axis=-1,
-    )
-
-    # keep cells that cross the dateline centered around 180
-    polygons_to_fix = np.any(recentered < -100, axis=-1) & np.any(
-        recentered > 100, axis=-1
-    )
-    result = np.where(
-        polygons_to_fix[:, None] & (recentered < 0), recentered + 360, recentered
-    )
-
-    return result
-
-
-def cell_ids2vertices(cell_ids, level, indexing_scheme):
-    import healpy as hp
-
-    nest = indexing_scheme == "nested"
-    nside = 2**level
-
-    boundary_vectors = hp.boundaries(nside, cell_ids, step=1, nest=nest)
-    lon, lat = np.reshape(
-        hp.vec2ang(np.moveaxis(boundary_vectors, 1, -1), lonlat=True), (2, -1, 4)
-    )
-
-    lon_ = center_around_prime_meridian(lon, lat)
-
-    return np.reshape(np.stack((lon_, lat), axis=-1), (-1, 4, 2))
-
-
-def cell_ids2cell_boundaries_geoarrow(cell_ids, level, indexing_scheme):
-    from arro3.core import list_array
-
-    vertices = cell_ids2vertices(cell_ids, level, indexing_scheme)
-    boundaries = np.concatenate([vertices, vertices[:, :1, :]], axis=1)
-
-    coords = np.reshape(boundaries, (-1, 2))
-    coords_per_pixel = boundaries.shape[1]
-    geom_offsets = np.arange(cell_ids.size + 1, dtype="int32")
-    ring_offsets = geom_offsets * coords_per_pixel
-
-    polygon_array = list_array(geom_offsets, list_array(ring_offsets, coords))
-    # We need to tag the array with extension metadata (`geoarrow.polygon`) so that Lonboard knows that this is a geospatial column.
-    polygon_array_with_geo_meta = polygon_array.cast(
-        polygon_array.field.with_metadata(
-            {
-                "ARROW:extension:name": "geoarrow.polygon",
-                "ARROW:extension:metadata": '{"crs": "epsg:4326"}',
-            }
-        )
-    )
-    return polygon_array_with_geo_meta
-
-
 def geoarrow2table(polygons):
     from arro3.core import Array, Schema, Table
 
@@ -110,9 +39,7 @@ def explore(
     cell_ids = arr.dggs.coord.data
     grid_info = arr.dggs.grid_info
 
-    polygons = cell_ids2cell_boundaries_geoarrow(
-        cell_ids, level=grid_info.resolution, indexing_scheme=grid_info.indexing_scheme
-    )
+    polygons = grid_info.cell_boundaries(cell_ids, backend="geoarrow")
 
     var = arr.variable
 
