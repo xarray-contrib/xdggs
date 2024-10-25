ed64b34f8320bebfc83ee39cb224c2629aa5a967
Justus Magin <keewis@posteo.de>
2024-10-25 17:08:52 +0200

    document the h3 grid info class

--- xdggs/h3.py
+++ xdggs/h3.py
@@ -60,7 +60,17 @@ def polygons_geoarrow(wkb):
 
 @dataclass(frozen=True)
 class H3Info(DGGSInfo):
+    """
+    Grid information container for h3 grids.
+
+    Parameters
+    ----------
+    resolution : int
+        The resolution of the grid
+    """
+
     resolution: int
+    """int : The resolution of the grid"""
 
     valid_parameters: ClassVar[dict[str, Any]] = {"resolution": range(16)}
 
@@ -87,19 +97,79 @@ class H3Info(DGGSInfo):
         return cls(**params)
 
     def to_dict(self: Self) -> dict[str, Any]:
+        """
+        Dump the normalized grid parameters.
+
+        Returns
+        -------
+        mapping : dict of str to any
+            The normalized grid parameters.
+        """
         return {"grid_name": "h3", "resolution": self.resolution}
 
     def cell_ids2geographic(
         self, cell_ids: np.ndarray
     ) -> tuple[np.ndarray, np.ndarray]:
+        """
+        Convert cell ids to geographic coordinates
+
+        Parameters
+        ----------
+        cell_ids : array-like
+            Array-like containing the cell ids.
+
+        Returns
+        -------
+        lon : array-like
+            The longitude coordinate values of the grid cells in degree
+        lat : array-like
+            The latitude coordinate values of the grid cells in degree
+        """
         lat, lon = cells_to_coordinates(cell_ids, radians=False)
 
         return lon, lat
 
     def geographic2cell_ids(self, lon, lat):
+        """
+        Convert cell ids to geographic coordinates
+
+        This will perform a binning operation: any point within a grid cell will be assign
+        that cell's ID.
+
+        Parameters
+        ----------
+        lon : array-like
+            The longitude coordinate values in degree
+        lat : array-like
+            The latitude coordinate values in degree
+
+        Returns
+        -------
+        cell_ids : array-like
+            Array-like containing the cell ids.
+        """
         return coordinates_to_cells(lat, lon, self.resolution, radians=False)
 
     def cell_boundaries(self, cell_ids, backend="shapely"):
+        """
+        Derive cell boundary polygons from cell ids
+
+        Parameters
+        ----------
+        cell_ids : array-like
+            The cell ids.
+        backend : {"shapely", "geoarrow"}, default: "shapely"
+            The backend to convert to.
+
+        Returns
+        -------
+        polygons : array-like
+            The derived cell boundary polygons. The format differs based on the passed
+            backend:
+
+            - ``"shapely"``: return a array of :py:class:`shapely.Polygon` objects
+            - ``"geoarrow"``: return a ``geoarrow`` array
+        """
         # TODO: convert cell ids directly to geoarrow once h3ronpy supports it
         wkb = cells_to_wkb_polygons(cell_ids, radians=False, link_cells=False)
 
