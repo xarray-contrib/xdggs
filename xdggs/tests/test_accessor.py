32c2fb7ea9b98de7d3c827a9717f9245bae2b8af
Justus Magin <keewis@users.noreply.github.com>
2025-01-18 10:50:47 +0100

    allow overriding the grid info (#63)
    * accessor method for decoding while possibly overriding the grid info
    
    * consolidate the treatment of `grid_info`
    
    * docstring for `decode`
    
    * don't modify the accessor
    
    * add a test for `decode`
    
    * expose DGGSInfo
    
    * add the rotation
    
    * Revert "add the rotation"
    
    This reverts commit df221522f5131bf86a3ab580d8fdc42a914c3e87.
    
    * expect `level` instead of `resolution`
    
    * forward to the accessor version
    
    * sync documentation between `decode` and the accessor's `decode`

--- xdggs/tests/test_accessor.py
+++ xdggs/tests/test_accessor.py
@@ -5,6 +5,90 @@ import xdggs
 
 
 @pytest.mark.parametrize(
+    ["obj", "grid_info", "name"],
+    (
+        pytest.param(
+            xr.Dataset(
+                coords={
+                    "cell_ids": (
+                        "cells",
+                        [1],
+                        {
+                            "grid_name": "healpix",
+                            "level": 1,
+                            "indexing_scheme": "ring",
+                        },
+                    )
+                }
+            ),
+            None,
+            None,
+            id="dataset-from attrs-standard name",
+        ),
+        pytest.param(
+            xr.DataArray(
+                [0.1],
+                coords={
+                    "cell_ids": (
+                        "cells",
+                        [1],
+                        {
+                            "grid_name": "healpix",
+                            "level": 1,
+                            "indexing_scheme": "ring",
+                        },
+                    )
+                },
+                dims="cells",
+            ),
+            None,
+            None,
+            id="dataarray-from attrs-standard name",
+        ),
+        pytest.param(
+            xr.Dataset(
+                coords={
+                    "zone_ids": (
+                        "zones",
+                        [1],
+                        {
+                            "grid_name": "healpix",
+                            "level": 1,
+                            "indexing_scheme": "ring",
+                        },
+                    )
+                }
+            ),
+            None,
+            "zone_ids",
+            id="dataset-from attrs-custom name",
+        ),
+        pytest.param(
+            xr.Dataset(coords={"cell_ids": ("cells", [1])}),
+            {"grid_name": "healpix", "level": 1, "indexing_scheme": "ring"},
+            None,
+            id="dataset-dict-standard name",
+        ),
+    ),
+)
+def test_decode(obj, grid_info, name) -> None:
+    kwargs = {}
+    if name is not None:
+        kwargs["name"] = name
+
+    if isinstance(grid_info, dict):
+        expected_grid_info = grid_info
+    elif isinstance(grid_info, xdggs.DGGSInfo):
+        expected_grid_info = grid_info.to_dict()
+    else:
+        expected_grid_info = obj[name if name is not None else "cell_ids"].attrs
+
+    actual = obj.dggs.decode(grid_info, **kwargs)
+    assert any(isinstance(index, xdggs.DGGSIndex) for index in actual.xindexes.values())
+    assert actual.dggs.grid_info.to_dict() == expected_grid_info
+
+
+@pytest.mark.parametrize(
     ["obj", "expected"],
     (
         (
