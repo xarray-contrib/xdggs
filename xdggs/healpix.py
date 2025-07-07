5bc8267f0163aa2e385c263995405d446226f8ea
Ola <98769588+ofk123@users.noreply.github.com>
2025-07-07 15:38:42 -0700

    small edit/typos (#157)
    * Update README.md
    
    typo
    
    * f""

--- xdggs/healpix.py
+++ xdggs/healpix.py
@@ -304,7 +304,7 @@ class HealpixInfo(DGGSInfo):
 
         backend_func = backends.get(backend)
         if backend_func is None:
-            raise ValueError("invalid backend: {backend!r}")
+            raise ValueError(f"invalid backend: {backend!r}")
 
         return backend_func(vertices)
 
