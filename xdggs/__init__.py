ba64bc5659a27fb8c593b71acef8a7df0075ab71
Justus Magin <keewis@users.noreply.github.com>
2025-08-25 16:20:16 +0200

    migrate to pixi (#167)
    * move the build system config below project config
    
    * replace setuptools with hatchling
    
    * initial pixi setup
    
    * configure the nightly env
    
    * configure the CI envs
    
    * configure the docs env
    
    * tasks for the docs env
    
    * configure a dev env
    
    * new syntax for ignoring in automatic release notes
    
    * use `pixi` in CI
    
    * use `pixi` on RTD
    
    * remove the old ci files
    
    * ignore pixi.lock
    
    * enable macos and windows for CI
    
    * add `pixi.lock` to accelerate CI
    
    * update `pixi.lock`
    
    * ignore the automatically created version file
    
    * ignore any `jupyter_execute` dirs
    
    * add `.gitattributes`

--- xdggs/__init__.py
+++ xdggs/__init__.py
@@ -1,17 +1,10 @@
-from importlib.metadata import PackageNotFoundError, version
-
 import xdggs.tutorial  # noqa: F401
+from xdggs._version import __version__
 from xdggs.accessor import DGGSAccessor  # noqa: F401
 from xdggs.grid import DGGSInfo
 from xdggs.h3 import H3Index, H3Info
 from xdggs.healpix import HealpixIndex, HealpixInfo
 from xdggs.index import DGGSIndex, decode
-
-try:
-    __version__ = version("xdggs")
-except PackageNotFoundError:  # noqa # pragma: no cover
-    # package is not installed
-    __version__ = "9999"
 
 __all__ = [
     "__version__",
