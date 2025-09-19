958b9746c4d256ddd6fd082cedb0e4aad3dd3b0c
Justus Magin <keewis@users.noreply.github.com>
2025-09-19 13:58:54 +0200

    healpix moc index (#151)
    * create a xarray healpix moc index
    
    * hack to allow creating a moc index
    
    * depend on `healpix-geo`
    
    * install `healpix-geo` from PyPI
    
    * install `healpix-geo` in the docs
    
    * use a special `values` method to access the actual data
    
    * only support `"nested"` in the MOC index
    
    * special-case the full domain
    
    * remove the duplicate index creation
    
    * forward to the wrapped index' `create_variables`
    
    * dask support in `create_variables`
    
    * support `dask` in `from_cell_ids`
    
    (this doesn't work yet because `moc::RangeMOC` does not derive from
    `serde`'s `Serialize` and `Deserialize`)
    
    * wrap the chunked delayed construction in a list
    
    * replace the dask loading code with a error message
    
    * out-of-core implement the RangeMOCIndex
    
    * remove the `n_partitions` parameter for now
    
    * forward the `size` and `nbytes` properties
    
    * check that construction from an array works properly
    
    * dask testing utils
    
    * remove the unused `unique` scheme for now
    
    * typo
    
    * changelog
    
    * treat `decode` as a method
    
    * document the `index_kind` parameter
    
    * pass through the `index_kind` parameter
    
    * rename `_pd_index` to `_index`
    
    * check `isel`
    
    * raise on irregular chunks
    
    * check `create_variables`
    
    * check that irregular chunks are rejected
    
    * more accurate condition
    
    * check more invalid chunks
    
    * check more valid chunk sizes
    
    * modify the chunksize if we reach the end
    
    * support irregular chunks by tracking them on the index
    
    * require `numpy>=2`
    
    * more (and smaller) tests
    
    * restructure the `isel` tests
    
    * improve the `isel` test
    
    * fix two bugs in the chunk subsetting
    
    * remove the default for the `chunksizes` parameter
    
    * depth → level
    
    * tests for `from_variables`
    
    * more tests for edge cases
    
    * check that the index is created properly
    
    * check the extracted chunks
    
    * explicitly raise an error for non-1D cell id coords
    
    * properly extract the chunks
    
    * also replace the typing to `xr.Index`
    
    * accept index options instead of specific args in `decode`
    
    * remove support for the `index_kind` kwarg for H3
    
    * mark the `raise_if_dask_computes` as vendored from `xarray`
    
    * docstrings for the moc index wrapper
    
    * point towards `healpix_geo.nested.RangeMOCIndex`
    
    * point out this only works for `nested`
    
    * more comments on the role of the different index objects
    
    * figure out chunk sizes after indexing
    
    * make sure we only pass uint64 arrays to the low-level index
    
    * check that the array indexer works
    
    * fix bugs introduced into `isel`
    
    * temporarily remove osx-arm64
    
    * add healpix-geo as a dependency
    
    * update lock file
    
    * only change the index name for `PandasIndex`
    
    * remove the outdated note
    
    * implement `HealpixMocIndex.sel`
    
    * tests for `HealpixMocIndex.sel`
    
    * install `dask` into the test env
    
    * get all the tests to pass
    
    * remove debug prints
    
    * update lock file
    
    * check that the error is raised properly
    
    * add back osx-arm64
    
    * bump lockfile

--- xdggs/h3.py
+++ xdggs/h3.py
@@ -20,7 +20,6 @@ except ImportError:
         cells_to_wkb_polygons,
         coordinates_to_cells,
     )
-from xarray.indexes import PandasIndex
 
 from xdggs.grid import DGGSInfo, translate_parameters
 from xdggs.index import DGGSIndex
@@ -213,7 +212,7 @@ class H3Index(DGGSIndex):
 
     def __init__(
         self,
-        cell_ids: Any | PandasIndex,
+        cell_ids: Any | xr.Index,
         dim: str,
         grid_info: DGGSInfo,
     ):
@@ -236,8 +235,8 @@ class H3Index(DGGSIndex):
     def grid_info(self) -> H3Info:
         return self._grid
 
-    def _replace(self, new_pd_index: PandasIndex):
-        return type(self)(new_pd_index, self._dim, self._grid)
+    def _replace(self, new_index: xr.Index):
+        return type(self)(new_index, self._dim, self._grid)
 
     def _repr_inline_(self, max_width: int):
         return f"H3Index(level={self._grid.level})"
--- xdggs/index.py
+++ xdggs/index.py
@@ -9,7 +9,7 @@ from xdggs.grid import DGGSInfo
 from xdggs.utils import GRID_REGISTRY, _extract_cell_id_variable
 
 
-def decode(ds, grid_info=None, *, name="cell_ids"):
+def decode(ds, grid_info=None, *, name="cell_ids", index_options=None, **index_kwargs):
     """
     decode grid parameters and create a DGGS index
 
@@ -23,6 +23,8 @@ def decode(ds, grid_info=None, *, name="cell_ids"):
         the dataset.
     name : str, default: "cell_ids"
         The name of the coordinate containing the cell ids.
+    index_options, **index_kwargs : dict, optional
+        Additional options to forward to the index.
 
     Returns
     -------
@@ -34,20 +36,25 @@ def decode(ds, grid_info=None, *, name="cell_ids"):
     xarray.Dataset.dggs.decode
     xarray.DataArray.dggs.decode
     """
-    return ds.dggs.decode(name=name, grid_info=grid_info)
+    if index_options is None:
+        index_options = {}
+
+    return ds.dggs.decode(
+        name=name, grid_info=grid_info, index_options=index_options | index_kwargs
+    )
 
 
 class DGGSIndex(Index):
     _dim: str
-    _pd_index: PandasIndex
+    _index: xr.Index
 
-    def __init__(self, cell_ids: Any | PandasIndex, dim: str, grid_info: DGGSInfo):
+    def __init__(self, cell_ids: Any | xr.Index, dim: str, grid_info: DGGSInfo):
         self._dim = dim
 
-        if isinstance(cell_ids, PandasIndex):
-            self._pd_index = cell_ids
+        if isinstance(cell_ids, xr.Index):
+            self._index = cell_ids
         else:
-            self._pd_index = PandasIndex(cell_ids, dim)
+            self._index = PandasIndex(cell_ids, dim)
 
         self._grid = grid_info
 
@@ -66,37 +73,41 @@ class DGGSIndex(Index):
             raise ValueError(f"unknown DGGS grid name: {grid_name}")
 
         index = cls.from_variables(variables, options=options)
-        index._pd_index.index.name = name
+        if isinstance(index._index, PandasIndex):
+            index._index.index.name = name
 
         return index
+
+    def values(self):
+        return self._index.index.values
 
     def create_variables(
         self, variables: Mapping[Any, xr.Variable] | None = None
     ) -> dict[Hashable, xr.Variable]:
-        return self._pd_index.create_variables(variables)
+        return self._index.create_variables(variables)
 
     def isel(
         self: "DGGSIndex", indexers: Mapping[Any, int | np.ndarray | xr.Variable]
     ) -> Union["DGGSIndex", None]:
-        new_pd_index = self._pd_index.isel(indexers)
-        if new_pd_index is not None:
-            return self._replace(new_pd_index)
+        new_index = self._index.isel(indexers)
+        if new_index is not None:
+            return self._replace(new_index)
         else:
             return None
 
     def sel(self, labels, method=None, tolerance=None):
         if method == "nearest":
             raise ValueError("finding nearest grid cell has no meaning")
-        return self._pd_index.sel(labels, method=method, tolerance=tolerance)
+        return self._index.sel(labels, method=method, tolerance=tolerance)
 
-    def _replace(self, new_pd_index: PandasIndex):
+    def _replace(self, new_index: PandasIndex):
         raise NotImplementedError()
 
     def cell_centers(self) -> tuple[np.ndarray, np.ndarray]:
-        return self._grid.cell_ids2geographic(self._pd_index.index.values)
+        return self._grid.cell_ids2geographic(self.values())
 
     def cell_boundaries(self) -> np.ndarray:
-        return self.grid_info.cell_boundaries(self._pd_index.index.values)
+        return self.grid_info.cell_boundaries(self.values())
 
     def zoom_to(self, level: int) -> np.ndarray:
         return self._grid.zoom_to(self._pd_index.index.values, level=level)
