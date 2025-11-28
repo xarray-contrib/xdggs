0277d1d9f6093084fd6d31e9467152edfd2b9fde
Justus Magin <keewis@posteo.de>
2025-11-28 12:03:36 +0100

    also check the `dict` versions

--- xdggs/tests/strategies.py
+++ xdggs/tests/strategies.py
@@ -15,13 +15,19 @@ names = st.none() | st.text(
     alphabet=st.sampled_from(string.ascii_letters + string.digits),
     min_size=1,
 )
+
+_ellipsoids = st.builds(
+    ellipsoid.Ellipsoid,
+    semimajor_axis=axis_sizes,
+    inverse_flattening=axis_sizes,
+    name=names,
+)
+_spheres = st.builds(ellipsoid.Sphere, radius=axis_sizes, name=names)
+
 ellipsoids = st.one_of(
     st.sampled_from(["WGS84", "airy", "bessel", "sphere", "unitsphere"]),
-    st.builds(ellipsoid.Sphere, radius=axis_sizes, name=names),
-    st.builds(
-        ellipsoid.Ellipsoid,
-        semimajor_axis=axis_sizes,
-        inverse_flattening=axis_sizes,
-        name=names,
-    ),
+    _ellipsoids,
+    _spheres,
+    _ellipsoids.map(lambda x: x.to_dict()),
+    _spheres.map(lambda x: x.to_dict()),
 )
