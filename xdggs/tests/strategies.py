4970e8c7582c03b2cac3dbdf5f6d85a6bcd25ca1
Justus Magin <keewis@posteo.de>
2025-11-28 14:12:37 +0100

    tune the ellipsoid strategy

--- xdggs/tests/strategies.py
+++ xdggs/tests/strategies.py
@@ -1,39 +1,41 @@
-import string
-
 import hypothesis.strategies as st
 
 from xdggs import ellipsoid
 
 axis_sizes = st.floats(
-    min_value=0,
+    min_value=0.0,
     allow_nan=False,
     allow_infinity=False,
     allow_subnormal=False,
     exclude_min=True,
 )
-names = st.none() | st.text(
-    alphabet=st.sampled_from(string.ascii_letters + string.digits),
-    min_size=1,
+inverse_flattening = st.floats(
+    min_value=5,
+    allow_nan=False,
+    allow_infinity=False,
+    allow_subnormal=False,
+    exclude_min=True,
 )
+
+names = st.none() | st.sampled_from(["WGS84", "airy", "bessel", "sphere", "unitsphere"])
 
 _ellipsoids = st.builds(
     ellipsoid.Ellipsoid,
     semimajor_axis=axis_sizes,
-    inverse_flattening=axis_sizes,
+    inverse_flattening=inverse_flattening,
     name=names,
 )
 _spheres = st.builds(ellipsoid.Sphere, radius=axis_sizes, name=names)
 
 
 def ellipsoids(variant="all"):
-    named = st.sampled_from(["WGS84", "airy", "bessel", "sphere", "unitsphere"])
     serialized = _ellipsoids.map(lambda x: x.to_dict()) | _spheres.map(
         lambda x: x.to_dict()
     )
     in_memory = _ellipsoids | _spheres
 
     variants = {
-        "serialized_only": named | serialized,
-        "in_memory_only": named | in_memory,
+        "serialized_only": names | serialized,
+        "in_memory_only": names | in_memory,
     }
-    return variants.get(variant, named | serialized | in_memory)
+    return variants.get(variant, names | serialized | in_memory)
