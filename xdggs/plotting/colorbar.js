0c3f95e608801ad7f5534cc688f5777afef61c95
Justus Magin <keewis@posteo.de>
2025-12-05 18:18:59 +0100

    styling of the colorbar

--- xdggs/plotting/colorbar.js
+++ xdggs/plotting/colorbar.js
@@ -71,7 +71,7 @@ function render({ model, el }) {
   const width = 640;
   const marginTop = 10;
   const marginRight = 20;
-  const marginBottom = 10;
+  const marginBottom = 40;
   const marginLeft = 40;
   const rampHeight = 40;
   const labelSpacer = 20;
@@ -84,33 +84,47 @@ function render({ model, el }) {
     .domain([vmin, vmax])
     .range([marginLeft, width - marginRight]);
 
-  const svg = d3.select(el).append("svg").attr("width", width);
+  const svg = d3
+    .select(el)
+    .append("svg")
+    .attr("width", width)
+    .attr("height", 400);
+  // axis label
+  const label = svg
+    .append("text")
+    .attr("text-anchor", "middle")
+    .attr("x", width / 2)
+    .attr("y", marginTop + 5)
+    .text(model.get("label"));
+  const labelHeight = label.node().getBBox().height;
 
   // ramp
   let image = svg
     .append("g")
-    .attr("transform", `translate(${marginLeft + 1}, ${marginTop})`)
+    .attr(
+      "transform",
+      `translate(${marginLeft + 1}, ${marginTop + labelHeight + labelSpacer})`,
+    )
     .call((image) => draw_ramp(image, colors, widget_width, rampHeight));
 
   // ticks and tick labels
   const xaxis = svg
     .append("g")
-    .attr("transform", `translate(0,${marginTop + rampHeight})`)
+    .attr(
+      "transform",
+      `translate(0,${marginTop + labelHeight + labelSpacer + rampHeight})`,
+    )
     .call(d3.axisBottom(x));
   const axisHeight = xaxis.node().getBBox().height;
 
-  // axis label
-  const labelY = marginTop + rampHeight + axisHeight + labelSpacer;
-  const label = svg
-    .append("text")
-    .attr("text-anchor", "middle")
-    .attr("x", width / 2)
-    .attr("y", labelY)
-    .text(model.get("label"));
-  const labelHeight = label.node().getBBox().height;
-
   // final styling
-  const finalHeight = labelY + labelHeight + marginBottom;
+  const finalHeight =
+    marginTop +
+    labelHeight +
+    labelSpacer +
+    rampHeight +
+    axisHeight +
+    marginBottom;
   svg.attr("height", finalHeight);
 
   model.on("change:label", (model, new_label) => label.text(new_label));
