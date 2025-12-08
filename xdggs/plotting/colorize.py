92c87279544de5090d06c29b064230837cfeca9f
Justus Magin <keewis@posteo.de>
2025-12-08 13:44:34 +0100

    add `robust` to the colorize options

--- xdggs/plotting/colorize.py
+++ xdggs/plotting/colorize.py
@@ -12,8 +12,8 @@ def normalize(var, params):
         halfrange = np.abs(var - params.center).max(skipna=True)
         normalizer = CenteredNorm(vcenter=params.center, halfrange=halfrange)
     else:
-        vmin = params.vmin if params.vmin is not None else var.min(skipna=True)
-        vmax = params.vmax if params.vmax is not None else var.max(skipna=True)
+        vmin = params.derive_limit(var, kind="vmin")
+        vmax = params.derive_limit(var, kind="vmax")
         normalizer = Normalize(vmin=vmin, vmax=vmax)
 
     stats = {"vmin": normalizer.vmin, "vmax": normalizer.vmax}
@@ -34,8 +34,8 @@ def extract_colors(cmap: matplotlib.colors.Colormap):
 class ColorizeParameters:
     vmin: float | None = None
     vmax: float | None = None
-
     center: float | None = None
+    robust: bool = False
 
     alpha: float | None = None
     cmap: str | matplotlib.colors.Colormap = "viridis"
@@ -47,3 +47,19 @@ class ColorizeParameters:
     @classmethod
     def from_dict(cls, mapping: dict[str, Any]) -> Self:
         return cls(**mapping)
+
+    def derive_limit(self, var, *, kind):
+        kinds = {
+            "vmin": ("min", 0.02),
+            "vmax": ("max", 0.98),
+        }
+
+        stored = getattr(self, kind, None)
+        if stored is not None:
+            return stored
+
+        method, quantile = kinds[kind]
+        if self.robust:
+            return var.quantile(q=quantile, skipna=True)
+        else:
+            return getattr(var, method)(skipna=True)
