b4ead8c21880c4afc0040baa179f85dba812b034
Justus Magin <keewis@posteo.de>
2025-12-19 14:06:26 +0100

    docstrings and type hints for the base class

--- xdggs/conventions/base.py
+++ xdggs/conventions/base.py
@@ -14,10 +14,53 @@ class Convention:
         grid_info: DGGSInfo | None,
         name: Hashable | None,
         index_options: dict[str, Any] | None,
-    ):
+    ) -> xr.Dataset:
+        """
+        Decode the dataset according to the convention.
+
+        This takes a dataset and performs all the necessary mutations to convert
+        to the xdggs convention and attach the DGGSIndex.
+
+        Parameters
+        ----------
+        ds : xr.Dataset
+            The encoded dataset.
+        grid_info : mapping or DGGSInfo, optional
+             Overrides for the grid metadata.
+        name : str, optional
+            The name of the cell ids coordinate.
+        index_options : mapping of str to Any, optional
+            Additional options for the index.
+
+        Returns
+        -------
+        decoded : xr.Dataset
+            The decoded dataset with a DGGSIndex.
+        """
         raise NotImplementedError
 
     def encode(
         self, obj: xr.Dataset | xr.DataArray, *, encoding: dict[str, Any] | None = None
-    ):
+    ) -> xr.Dataset:
+        """
+        Encode according to the convention.
+
+        This takes a dataset with a DGGSIndex and performs the necessary
+        mutations to convert from the xdggs convention to the convention in
+        question.
+
+        Note that this must drop the DGGSIndex.
+
+        Parameters
+        ----------
+        ds : xr.Dataset
+            The dataset to encode. Must have a DGGSIndex.
+        encoding : mapping of str to Any, optional
+            Additional options for the convention.
+
+        Returns
+        -------
+        encoded : xr.Dataset
+            The encoded dataset.
+        """
         raise NotImplementedError
