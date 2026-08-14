bc684200fd2cc3c8268b969afa59ee4118365bcd
Justus Magin <keewis@posteo.de>
2026-08-15 00:28:47 +0200

    exclude jupyter_execute

--- docs/conf.py
+++ docs/conf.py
@@ -52,7 +52,7 @@ templates_path = ["_templates", sphinx_autosummary_accessors.templates_path]
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ["_build", "directory"]
+exclude_patterns = ["_build", "directory", "jupyter_execute"]
 
 ## Github Buttons
 html_theme_options = {
