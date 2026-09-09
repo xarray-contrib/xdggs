edd2786c40063f8c15df5c6b92df0d0ed95b4ab9
Justus Magin <keewis@posteo.de>
2026-09-09 16:48:29 +0200

    test encoding as compressed cell ids

--- xdggs/tests/test_conventions_zarr.py
+++ xdggs/tests/test_conventions_zarr.py
@@ -4,7 +4,7 @@ import xarray as xr
 
 import xdggs
 from xdggs.conventions.zarr import Zarr
-from xdggs.tests import assert_indexes_equal
+from xdggs.tests import assert_indexes_equal, requires_healpix_geo_0_4_1
 
 
 def translate(mapping):
@@ -193,3 +193,146 @@ def test_encode(grid_info, metadata_object, cell_ids, name, dim):
 
     xr.testing.assert_identical(encoded, expected)
     assert_indexes_equal(encoded.xindexes, expected.xindexes)
+
+
+@pytest.mark.parametrize(
+    ["metadata_object", "encoding", "grid_info", "cell_ids", "encoded"],
+    (
+        pytest.param(
+            {
+                "name": "healpix",
+                "refinement_level": 10,
+                "indexing_scheme": "nested",
+                "coordinate": "cell_ranges",
+                "spatial_dimension": "cells",
+                "compression": "ranges",
+            },
+            {"coordinate": "cell_ranges", "compression": "ranges"},
+            {"grid_name": "healpix", "level": 10, "indexing_scheme": "nested"},
+            xr.Variable(
+                "cells",
+                np.array(
+                    [
+                        112,
+                        113,
+                        114,
+                        115,
+                        116,
+                        117,
+                        118,
+                        119,
+                        120,
+                        121,
+                        122,
+                        123,
+                        124,
+                        125,
+                        126,
+                        127,
+                        1152,
+                        1153,
+                        1154,
+                        1155,
+                        1156,
+                        1157,
+                        1158,
+                        1159,
+                        1160,
+                        1161,
+                        1162,
+                        1163,
+                        1164,
+                        1165,
+                        1166,
+                        1167,
+                    ],
+                    dtype="uint64",
+                ),
+            ),
+            xr.Variable(
+                ("range_index", "bounds"),
+                np.array(
+                    [
+                        [30786325577728, 35184372088832],
+                        [316659348799488, 321057395310592],
+                    ]
+                ),
+            ),
+            id="ranges-10",
+        ),
+        pytest.param(
+            {
+                "name": "healpix",
+                "refinement_level": 2,
+                "indexing_scheme": "nested",
+                "coordinate": "compacted_cell_ids",
+                "spatial_dimension": "cells",
+                "compression": "compacted",
+            },
+            {
+                "coordinate": "compacted_cell_ids",
+                "compression": "compacted",
+                "compacted_level": 1,
+            },
+            {"grid_name": "healpix", "level": 2, "indexing_scheme": "nested"},
+            xr.Variable(
+                "cells",
+                np.array([4, 5, 6, 7, 20, 21, 22, 23, 32, 33, 34, 35]),
+            ),
+            xr.Variable(
+                ("compacted_cells"),
+                np.array([216172782113783808, 792633534417207296, 1224979098644774912]),
+            ),
+            id="compacted_flat-5",
+        ),
+        pytest.param(
+            {
+                "name": "healpix",
+                "refinement_level": 5,
+                "indexing_scheme": "nested",
+                "coordinate": "compacted_cell_ids",
+                "spatial_dimension": "cells",
+                "compression": "compacted",
+            },
+            {"coordinate": "compacted_cell_ids", "compression": "compacted"},
+            {"grid_name": "healpix", "level": 5, "indexing_scheme": "nested"},
+            xr.Variable("cells", np.array([4, 5, 6, 7, 8, 10])),
+            xr.Variable(
+                ("compacted_cells"),
+                np.array([216172782113783808, 306244774661193728, 378302368699121664]),
+            ),
+            marks=requires_healpix_geo_0_4_1,
+            id="compacted-5",
+        ),
+    ),
+)
+def test_encode_compression(metadata_object, encoding, grid_info, cell_ids, encoded):
+    convention = Zarr()
+
+    var = cell_ids.copy()
+    var.attrs = grid_info
+    index = xdggs.index.DGGSIndex.from_variables(
+        {"cell_ids": var},
+        options={
+            "index_kind": "moc",
+        },
+    )
+    ds = xr.Dataset(
+        {"data": ("cells", np.arange(cell_ids.size))},
+        coords=xr.Coordinates.from_xindex(index),
+    )
+
+    name = metadata_object["coordinate"]
+    expected = xr.Dataset(
+        {name: coord.variable for name, coord in ds.data_vars.items()},
+        coords={name: encoded},
+        attrs={
+            "zarr_conventions": [convention.convention_metadata],
+            "dggs": metadata_object,
+        },
+    )
+
+    actual = convention.encode(ds, encoding=encoding)
+
+    xr.testing.assert_equal(actual, expected)
+    assert_indexes_equal(actual.xindexes, expected.xindexes)
