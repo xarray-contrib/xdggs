634f4064861e050ae4b0a49349883c1b6cf9b2ea
Justus Magin <keewis@posteo.de>
2026-09-22 18:11:38 +0200

    typing and docstring updates

--- xdggs/accessor.py
+++ xdggs/accessor.py
@@ -11,6 +11,7 @@ from xdggs.index import DGGSIndex
 from xdggs.plotting import explore
 
 if TYPE_CHECKING:
+    from collections.abc import Mapping
     from typing import Any
 
     from lonboard.basemap import MaplibreBasemap
@@ -45,11 +46,11 @@ class DGGSAccessor:
 
     def decode(
         self,
-        grid_info=None,
+        grid_info: Mapping[str, object] | DGGSInfo | None = None,
         *,
-        name="cell_ids",
-        convention="xdggs",
-        index_options=None,
+        name: str = "cell_ids",
+        convention: str = "xdggs",
+        index_options: Mapping[str, Any] | None = None,
         **index_kwargs,
     ) -> xr.Dataset | xr.DataArray:
         """decode the DGGS cell ids
@@ -68,13 +69,15 @@ class DGGSAccessor:
             - "xdggs": the existing xdggs convention. ``name`` points to the
               coordinate containing cell ids, and which has all the grid
               metadata. The ``name`` parameter defaults to ``"cell_ids"``.
-            - "cf": the upcoming CF convention standardization. While the
+            - "cf": the DGGS section from the CF conventions. While the
               convention extension is specialized on ``healpix`` for now, the
               decoder can work with other DGGS as well. For this, all metadata
               lives on a variable with a ``grid_mapping_name`` attribute, and
               the cell ids coordinate is indicated by the ``coordinates``
               attribute on data variables / other coordinates (this can be
               overridden by the ``name`` parameter).
+            - "zarr": the zarr dggs convention. All metadata is contained in a
+              single nested JSON object that lives in the group attributes.
         index_options, **index_kwargs : dict, optional
             Additional options to forward to the index.
 
